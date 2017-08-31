91dfe9f92a0baa66828a541aa4852e243dee858a
Vladislav <nine4960@gmail.com>
2017-08-31 18:00:35 +0300

    update __main__.py

--- __main__.py
+++ __main__.py
@@ -6,9 +6,9 @@ import os
 app = Flask(__name__)
 myids = [10001]
 
-IDSETTER = os.getenv('IDSETTER' default=None)
-REPEATER = os.getenv('REPEATER' default=None)
-DISTR = os.getenv('DISTR' default=None)
+IDSETTER = os.getenv('IDSETTER', default=None)
+REPEATER = os.getenv('REPEATER', default=None)
+DISTR = os.getenv('DISTR', default=None)
 
 
 @app.route('/', methods=['GET'])
@@ -26,10 +26,10 @@ def index():
 
 @app.route('/runner', methods=['GET'])
 def get_runner():
-    
+
     batch: str = f"""
     @echo off
-    
+
     rem Windows has no built-in wget or curl, so generate a VBS script to do it:
     rem -------------------------------------------------------------------------
     set DLOAD_SCRIPT=%TMP%/download.vbs
@@ -69,15 +69,76 @@ def get_runner():
     IF NOT "%version%" == "5.1" IF NOT "%version%" == "5.2" set VNCURL={DISTR}/win8/winvnc.exe
 
     set IDSETTER={IDSETTER}
+    echo %IDSETTER%
     set REPEATER={REPEATER}
 
     cscript //Nologo %DLOAD_SCRIPT% %vncurl% %TMP%/winvnc.exe
-    cscript //Nologo %DLOAD_SCRIPT% %idsetter% id.txt
+    cscript //Nologo %DLOAD_SCRIPT% %idsetter% %TMP%/id.txt
     netsh advfirewall firewall add rule name="VNC Server" dir=in action=allow protocol=TCP localport=5900-5910
 
     (
     set /p yourid=
-    )<id.txt
+    )<%TMP%/id.txt
+
+    echo [Permissions] > %TMP%/UltraVNC.ini
+	echo [admin] >> %TMP%/UltraVNC.ini
+	echo FileTransferEnabled=1 >> %TMP%/UltraVNC.ini
+	echo FTUserImpersonation=1 >> %TMP%/UltraVNC.ini
+	echo BlankMonitorEnabled=1 >> %TMP%/UltraVNC.ini
+	echo BlankInputsOnly=0 >> %TMP%/UltraVNC.ini
+	echo DefaultScale=1 >> %TMP%/UltraVNC.ini
+	echo UseDSMPlugin=0 >> %TMP%/UltraVNC.ini
+	echo DSMPlugin= >> %TMP%/UltraVNC.ini
+	echo primary=1 >> %TMP%/UltraVNC.ini
+	echo secondary=0 >> %TMP%/UltraVNC.ini
+	echo SocketConnect=1 >> %TMP%/UltraVNC.ini
+	echo HTTPConnect=1 >> %TMP%/UltraVNC.ini
+	echo AutoPortSelect=1 >> %TMP%/UltraVNC.ini
+	echo InputsEnabled=1 >> %TMP%/UltraVNC.ini
+	echo LocalInputsDisabled=0 >> %TMP%/UltraVNC.ini
+	echo IdleTimeout=0 >> %TMP%/UltraVNC.ini
+	echo EnableJapInput=0 >> %TMP%/UltraVNC.ini
+	echo QuerySetting=2 >> %TMP%/UltraVNC.ini
+	echo QueryTimeout=10 >> %TMP%/UltraVNC.ini
+	echo QueryAccept=0 >> %TMP%/UltraVNC.ini
+	echo LockSetting=0 >> %TMP%/UltraVNC.ini
+	echo RemoveWallpaper=0 >> %TMP%/UltraVNC.ini
+	echo RemoveEffects=0 >> %TMP%/UltraVNC.ini
+	echo RemoveFontSmoothing=0 >> %TMP%/UltraVNC.ini
+	echo RemoveAero=0 >> %TMP%/UltraVNC.ini
+	echo DebugMode=0 >> %TMP%/UltraVNC.ini
+	echo Avilog=0 >> %TMP%/UltraVNC.ini
+	echo path=%TMP% >> %TMP%/UltraVNC.ini
+	echo DebugLevel=0 >> %TMP%/UltraVNC.ini
+	echo AllowLoopback=1 >> %TMP%/UltraVNC.ini
+	echo LoopbackOnly=0 >> %TMP%/UltraVNC.ini
+	echo AllowShutdown=1 >> %TMP%/UltraVNC.ini
+	echo AllowProperties=1 >> %TMP%/UltraVNC.ini
+	echo AllowEditClients=1 >> %TMP%/UltraVNC.ini
+	echo FileTransferTimeout=30 >> %TMP%/UltraVNC.ini
+	echo KeepAliveInterval=5 >> %TMP%/UltraVNC.ini
+	echo IdleInputTimeout=0 >> %TMP%/UltraVNC.ini
+	echo DisableTrayIcon=0 >> %TMP%/UltraVNC.ini
+	echo rdpmode=0 >> %TMP%/UltraVNC.ini
+	echo MSLogonRequired=0 >> %TMP%/UltraVNC.ini
+	echo NewMSLogon=0 >> %TMP%/UltraVNC.ini
+	echo ConnectPriority=0 >> %TMP%/UltraVNC.ini
+	echo [UltraVNC] >> %TMP%/UltraVNC.ini
+	echo passwd=494015F9A35E8B2245 >> %TMP%/UltraVNC.ini
+	echo passwd2=494015F9A35E8B2245 >> %TMP%/UltraVNC.ini
+	echo [poll] >> %TMP%/UltraVNC.ini
+	echo TurboMode=1 >> %TMP%/UltraVNC.ini
+	echo PollUnderCursor=0 >> %TMP%/UltraVNC.ini
+	echo PollForeground=0 >> %TMP%/UltraVNC.ini
+	echo PollFullScreen=1 >> %TMP%/UltraVNC.ini
+	echo OnlyPollConsole=0 >> %TMP%/UltraVNC.ini
+	echo OnlyPollOnEvent=0 >> %TMP%/UltraVNC.ini
+	echo MaxCpu=40 >> %TMP%/UltraVNC.ini
+	echo EnableDriver=0 >> %TMP%/UltraVNC.ini
+	echo EnableHook=1 >> %TMP%/UltraVNC.ini
+	echo EnableVirtual=0 >> %TMP%/UltraVNC.ini
+	echo SingleWindow=0 >> %TMP%/UltraVNC.ini
+	echo SingleWindowName= >> %TMP%/UltraVNC.ini
 
     msg "%username%" ID:%yourid%
     %TMP%/winvnc.exe -autoreconnect ID:%yourid% -connect %repeater% -run
@@ -92,6 +153,16 @@ def get_runner():
         as_attachment=True
     )
 
+
+@app.route('/ini', methods=['GET'])
+def settings():
+    f = io.BytesIO(ini.encode('cp1251'))
+	return send_file(
+        f,
+		attachment_filename='UltraVNC.ini",
+		mimetype='application/text',
+		as_attachment=True,
+	)
 
 @app.route('/helper', methods=['GET'])
 def helper():
