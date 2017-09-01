a90b99928e233d470fae1b2234abc197517f870a
Vladislav Katrich <30462664+SunKat1@users.noreply.github.com>
2017-09-01 16:46:47 +0300

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -155,4 +155,4 @@ def helper():
     return render_template('instruction.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
